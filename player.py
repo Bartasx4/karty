ca3d3884308ec8056fb60d1d31d4faa5c886a55f
Bartek <bartasx@hotmail.com>
2022-11-09 11:50:51 +0100

    Check point before editing

--- player.py
+++ player.py
@@ -14,10 +14,10 @@ class Player:
         self.up_deck = Deck()
 
     def move(self):
-    	if self.hand_deck.count > 0:
-    		if self.__can_put_deck(self.hand_deck):
-    			self.__put_on_table(self.hand_deck)
-    	return True
+        if not self.hand_deck.empty:
+            if self.__can_put_deck(self.hand_deck):
+                self.__put_on_table(self.hand_deck)
+        return True
 
     def hand(self):
         print('Hand:')
@@ -25,28 +25,38 @@ class Player:
         print('Cards up:')
         self.up_deck.print_all()
         print('')
-    
+
     def __can_put_card(self, card: Card):
-    	if self.table_deck.last <= card:
-    		return True
-    	return False
-    
+        if self.table_deck.last <= card:
+            return True
+        return False
+
     def __can_put_deck(self, player_hand: Deck):
-    	for in_hand in player_hand:
-    		if self.table_deck.last <= in_hand:
-    			return True
-    	return False
-    	
+        for in_hand in player_hand:
+            if self.table_deck.table.last <= in_hand:
+                return True
+        return False
+
+    def __last_chance(self):
+        if not self.table_deck.empty:
+            last_chance_card = self.table_deck.get()
+            if last_chance_card >= self.table_deck.table.last:
+                self.table_deck.add(last_chance_card)
+            else:
+                self.hand_deck.add(last_chance_card)
+                self.hand_deck.add(self.table_deck.table)
+
     def __put_on_table(self, player_deck):
-    	cards_can_put = []
-    	for card in player_deck:
-    		if self.__can_put_card(card):
-    			cards_can_put.append(card)
-    	to_put_index = min(cards_can_put, key=lambda card: card.points)
-    	to_put_card = cards_can_put[to_put_index]
-    	while card:= player_deck.get_card_by_value:
-    		self.table_deck.table.add(card)
-    		if self.table_deck.count > 0:
-    			player_deck.add(self.table_deck.get())
-    	
-    	
+        cards_can_put = []
+        for card in player_deck:
+            if self.__can_put_card(card):
+                cards_can_put.append(card)
+        to_put_index = min(cards_can_put, key=lambda card_: card_.points)
+        to_put_card = cards_can_put[to_put_index]
+        self.table_deck.table.add(player_deck.get_card_by_value(to_put_card))
+        if not self.table_deck.empty:
+            player_deck.add(self.table_deck.get())
+        while card := player_deck.get_card_by_value(self.table_deck.last):
+            self.table_deck.table.add(card)
+            if not self.table_deck.empty:
+                player_deck.add(self.table_deck.get())
--- table.py
+++ table.py
@@ -4,61 +4,60 @@ from player import Player
 
 
 class Table:
-	
-	def __init__(self):
-		values = [0, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14]
-		self.players = []
-		self.deck = Deck()
-		self.deck.set(values=values)
-		
-	def start(self, players=None, bot_count=3):
-		self.deck = Deck()
-		self.deck.new_deck().shuffle()
-		players = players if players else []
-		self.__create_players(bot_count)
-		random.shuffle(self.players)
-		self.__split_cards()
-		self.__game_loop()
-		
-	def __check_win(self):
-		for player in self.players:
-			empty = [player.hand_deck.count == 0,
-			player.up_deck.count == 0,
-			player.down_deck == 0]
-			if all(empty):
-				return player
-		return False
-		
-	def __create_players(self, players_count):
-		self.players = []
-		human = Player(table_deck=self.deck, nick='Bartek', computer=False)
-		self.players.append(human)
-		for i in range(players_count):
-		      bot = Player(table_deck=self.deck, nick=f'{i+1}', computer=True)
-		      self.players.append(bot)
-		      
-	def __end_game(self):
-		pass
-		      
-	def __game_loop(self):
-		for player in self.players:
-			while not player.move():
-				if self.__check_win():
-					return self.__end_game()
-				
-	def __last_four(self):
-		last_cards = self.deck[-4:]
-		last_one = self.deck.last
-		for card in last_cards:
-			same = Deck().is_same_dict(card, last_one)['value']
-			if not same:
-				return False
-		return True
-				    
-	def __split_cards(self):
-		for player in self.players:
-			stacks = [player.hand_deck, player.up_deck, player.down_deck]
-			for player_deck in stacks:
-				for _ in range(3):
-					player_deck.add(self.deck.card)
-					
+
+    def __init__(self):
+        values = [0, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14]
+        self.players = []
+        self.deck = Deck()
+        self.deck.set(values=values)
+
+    def start(self, players=None, bot_count=3):
+        self.deck = Deck()
+        self.deck.new_deck().shuffle()
+        players = players if players else []
+        self.__create_players(bot_count)
+        random.shuffle(self.players)
+        self.__split_cards()
+        self.__game_loop()
+
+    def __check_win(self):
+        for player in self.players:
+            empty = [player.hand_deck.count == 0,
+                     player.up_deck.count == 0,
+                     player.down_deck == 0]
+            if all(empty):
+                return player
+        return False
+
+    def __create_players(self, players_count):
+        self.players = []
+        human = Player(table_deck=self.deck, nick='Bartek', computer=False)
+        self.players.append(human)
+        for i in range(players_count):
+            bot = Player(table_deck=self.deck, nick=f'{i + 1}', computer=True)
+            self.players.append(bot)
+
+    def __end_game(self):
+        pass
+
+    def __game_loop(self):
+        for player in self.players:
+            while not player.move():
+                if self.__check_win():
+                    return self.__end_game()
+
+    def __last_four(self):
+        last_cards = self.deck[-4:]
+        last_one = self.deck.last
+        for card in last_cards:
+            same = Deck().is_same_dict(card, last_one)['value']
+            if not same:
+                return False
+        return True
+
+    def __split_cards(self):
+        for player in self.players:
+            stacks = [player.hand_deck, player.up_deck, player.down_deck]
+            for player_deck in stacks:
+                for _ in range(3):
+                    player_deck.add(self.deck.card)
